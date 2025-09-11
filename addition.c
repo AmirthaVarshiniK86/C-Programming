ca6f190e1fd267e8ad14d12942885c62506e5ebf
Amirtha Varshini K <amirthavarshini8608@gmail.com>
2025-09-11 19:46:05 +0530

    Updated addition.c to modern library elements

--- addition.c
+++ addition.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <conio.h>
-void main()
+int main()
 {
   int a,b,c;
-  clrscr();
   printf("Enter the first value to add:");
   scanf("%d",&a);
   printf("Enter the second value to add:");
   scanf("%d",&b);
   c=a+b;
-  printf("The sum of two numbers a and b is %d.",c);
-  getch();
+  printf("The sum of two numbers a and b is %d.\n",c);
+  return 0;
 }
