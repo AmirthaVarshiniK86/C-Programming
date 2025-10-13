c56ddf900c203cfc28da407f67cc7d8a6696b1f4
Amirtha Varshini K <amirthavarshini8608@gmail.com>
2025-10-13 19:12:59 +0530

    Created print_array.c

---
+++ print_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a[5]={10,20,30,40};
+  int i;
+  printf("The array elements are \n");
+  for (i=0; i<=3; i++)
+    {
+      printf("%d\n", a[i]);
+    }
+  return 0;
+}
